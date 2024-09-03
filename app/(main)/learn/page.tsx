707a214727c05b26df66122b23671e40a5c201b4
Satara2358 <25549072+satara2358@users.noreply.github.com>
2024-09-02 22:37:17 -0500

    conexion bd neon

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,7 +2,6 @@ import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { Header } from "./header";
 import { UserProgress } from "@/components/user-progress";
-import { title } from "process";
 
 const LearnPage = () => {
   return ( 
