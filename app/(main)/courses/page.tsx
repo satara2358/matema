e6472d4b64e4c5533f0bae370282215ef3b0ccb8
Satara2358 <25549072+satara2358@users.noreply.github.com>
2024-09-06 01:07:13 -0500

    Render Cursos

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -1,4 +1,5 @@
 import { getCourses } from "@/db/queries";
+import { List } from "./list";
 
 const CoursesPage = async () => {
   const data = await getCourses();
@@ -7,7 +8,11 @@ const CoursesPage = async () => {
       <h1 className="text-2xl font-bold text-neutral-700">
         Cursos
       </h1>
-      {JSON.stringify(data)}
+      {/* {JSON.stringify(data)} */}
+      <List
+        courses={data}
+        activeCourseId={1}
+      />
     </div>
     );
 }
