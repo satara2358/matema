0ae521a56bf3d2ede59cf4ad9f048ffaa812459b
Satara2358 <25549072+satara2358@users.noreply.github.com>
2024-12-21 01:13:56 -0500

    Schema units, challenges, lessons

--- db/schema.ts
+++ db/schema.ts
@@ -1,5 +1,5 @@
 import { relations } from "drizzle-orm";
-import { integer, pgTable, serial, text } from "drizzle-orm/pg-core";
+import { integer, pgEnum, pgTable, serial, text } from "drizzle-orm/pg-core";
 
 export const courses = pgTable("courses", {
     id: serial("id").primaryKey(),
@@ -7,7 +7,50 @@ export const courses = pgTable("courses", {
     imageSrc: text("image_src").notNull(),
 });
 
-export const coursesRelations = relations(courses, ({ many }) => ({userProgress: many(userProgress)}))
+export const coursesRelations = relations(courses, ({ many }) => ({
+    userProgress: many(userProgress),
+    units: many(units),
+}));
+
+export const units = pgTable("units", {
+    id: serial("id").primaryKey(),
+    title: text("title").notNull(),
+    description: text("description").notNull(),
+    courseId: integer("course_id").references(() => courses.id, { onDelete: "cascade" }).notNull(),
+    order: integer("order").notNull(),
+});
+
+export const unitsRelations = relations(units, ({ many, one }) => ({
+    course: one(courses, {
+        fields: [units.courseId],
+        references: [courses.id],
+    }),
+    lesson: many(lessons)
+}));
+
+export const lessons = pgTable("lessons", {
+    id: serial("id").primaryKey(),
+    title: text("title").notNull(),
+    unitId: integer("unit_id").references(() => units.id, { onDelete: "cascade" }).notNull(),
+    order: integer("order").notNull(),
+});
+
+export const lessonsRelations = relations(lessons, ({ many, one }) => ({
+    unit: one(units, {
+        fields: [lessons.unitId],
+        references: [units.id],
+    }),
+}));
+
+export const challengesEnum = pgEnum("type", ["SELECT", "ASSIST"]);
+
+export const challenges = pgTable("challenges", {
+    id: serial("id").primaryKey(),
+    lessonId: integer("lesson_id").references(() => lessons.id, { onDelete: "cascade" }).notNull(),
+    type: challengesEnum("type").notNull(),
+    question: text("question").notNull(),
+    order: integer("order").notNull(),
+});
 
 export const userProgress = pgTable("user_progress", {
     userId: text("user_id").primaryKey(),
